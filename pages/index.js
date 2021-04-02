70411aaa23ef1111606576f6fb15e9374ae5eb3c
caew <weac.buy@gmail.com>
2021-04-02 00:02:50 -0300

    teste sobre

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,17 @@
-import Link from 'next/Link';
+import Link from 'next/link';
 
-import{ useState } from 'react';
-
-function Home  () {
+function Home () {
     return (<div>
-            <h1>CAEW IS TOP 10</h1>
-            <link href="/sobre">
+            <h1>Home</h1>
+
+            <Link href="/Sobre">
                 <a>Acessar Página Sobre</a>
 
             </Link>
             
-        </div>
-    )
+    </div>)
 }
 
-function Contador () {
-            const [contador, setContador] = useState(1);
-            
-            function adicionarContador (){
-                setContador( contador + 1 );
-                }
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-     )
-}
-
-
-export default Home;
+export default Home
 
 
